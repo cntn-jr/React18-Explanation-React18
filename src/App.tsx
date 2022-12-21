c94f88b50433c2dc14511151fac4edb6c3acb5d8
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-12-21 15:30:25 +0900

    section2の内容の削除

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
 import { AutoBatchOther } from './components/AutoBatchOther';
 
 function App() {
-  useEffect(() => {
-    console.log('APPがレンダリングされた！！');
-  }, []);
   return (
     <div className="App">
       <AutoBatchEventHandler />
