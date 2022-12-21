f453bc3f181cd1dad1f0855813df6fc2e2d5ffe5
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2022-12-21 15:32:09 +0900

    section3の内容の削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
-import { AutoBatchOther } from './components/AutoBatchOther';
 
 function App() {
   useEffect(() => {
@@ -10,8 +8,20 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <AutoBatchEventHandler />
-      <AutoBatchOther />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
